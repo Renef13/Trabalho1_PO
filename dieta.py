0e06896078c3c85fcd418fc21a029016b4ffb4ae
Renef <renefricardo30@gmail.com>
2025-10-27 23:03:38 -0300

    atualizando versoes

---
+++ dieta.py
@@ -0,0 +1,33 @@
+from pyomo.environ import ConcreteModel, Var, Objective, Constraint, SolverFactory
+import pandas as pd
+
+model = ConcreteModel()
+
+foods = ['feijao']
+costs = [1.2]
+nutrients = ['proteina', 'carboidrato', 'vitamina']
+requirements = {'proteina': 50, 'carboidrato': 80, 'vitamina': 20}
+nutrition_values = {'feijao': [5, 20, 10]}
+
+model.x = Var(foods, domain=float)
+
+model.obj = Objective(expr=sum(costs[i] * model.x[foods[i]] for i in range(len(foods))), sense='min')
+
+
+def nutrient_constraints(model, nutrient):
+    index = nutrients.index(nutrient)
+    return sum(nutrition_values[food][index] * model.x[food] for food in foods) >= requirements[nutrient]
+
+
+model.nutrient_constraints = Constraint(nutrients, rule=nutrient_constraints)
+
+solver = SolverFactory('glpk')
+solver.solve(model)
+
+results = pd.DataFrame({
+    'Alimento': foods,
+    'Quantidade (porções)': [model.x[food].value for food in foods]
+})
+
+print("Resultado da Dieta:")
+print(results)
---
+++ mochila.py
@@ -0,0 +1,30 @@
+from pyomo.environ import ConcreteModel, Var, Objective, Constraint, SolverFactory, Binary
+import pandas as pd
+
+model = ConcreteModel()
+
+items = ['item1', 'item2', 'item3', 'item4', 'item5']
+values = [5000, 300, 2000, 1500, 50]  # Valores dos itens
+weights = [2.5, 0.8, 0.2, 0.6, 0.4]  # Pesos dos itens
+capacity = 3.0  # Capacidade da mochila
+
+model.x = Var(items, domain=Binary)
+
+model.obj = Objective(expr=sum(values[i] * model.x[items[i]] for i in range(len(items))), sense='max')
+
+model.capacity_constraint = Constraint(expr=sum(weights[i] * model.x[items[i]] for i in range(len(items))) <= capacity)
+
+# o modelo
+solver = SolverFactory('glpk')
+solver.solve(model)
+
+
+results = pd.DataFrame({
+    'Item': items,
+    'Valor': values,
+    'Peso': weights,
+    'Selecionado': [model.x[item].value for item in items]
+})
+
+print("Resultado da Mochila 0-1:")
+print(results)
